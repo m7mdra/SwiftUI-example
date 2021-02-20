d7bdae1168c20758a37d58b687caa5443c24ba89
m7mdra <xm7mdrax@gmail.com>
2021-02-20 02:48:06 +0200

    Initial Commit

---
+++ ProjectOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectOne
+//
+//  Created by Sharif on 20/02/2021.
+//  Copyright © 2021 Sharif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
