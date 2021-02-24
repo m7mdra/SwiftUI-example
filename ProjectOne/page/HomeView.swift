290390389bbf6b1cd193836be1b328550477d25d
m7mdra <xm7mdrax@gmail.com>
2021-02-24 03:57:17 +0200

    WIP home

--- ProjectOne/page/HomeView.swift
+++ ProjectOne/page/HomeView.swift
@@ -69,7 +69,7 @@ struct HomeView : View {
             Text("Ottoman Collection")
                 .foregroundColor(.black)
                 .font(.custom("Heavy", size: 30))
-                .padding(.top, 16)
+                .padding(.top, 8)
             Text("Find the perfect watch for your wrist")
                 .font(.custom("Book", size: 14))
                 .foregroundColor(Color(hex:"#676870"))
@@ -85,9 +85,9 @@ struct HomeView : View {
                     Text("See all")
                 }).accentColor(Color(hex:"#CC9D76"))
                 
-            }.padding()
+            }.padding(.horizontal)
             ScrollView(.horizontal,showsIndicators: false) {
-                HStack(spacing: 20) {
+                HStack(spacing: 10) {
                     ForEach(0..<CategoryData.list.count) {index in
                         
                         HomeCategory(data: CategoryData.list[index])
@@ -98,18 +98,37 @@ struct HomeView : View {
                 Text("Top deals")
                     .foregroundColor(.black)
                     .font(.custom("Heavy", size: 20))
-                    
+                
                 Spacer()
                 Button(action:{},label: {
                     Text("See all")
                 }).accentColor(Color(hex:"#CC9D76"))
                 
-            }.padding()
+            }.padding(.horizontal)
             ScrollView(.horizontal,showsIndicators: false) {
-                HStack() {
+                HStack {
                     ForEach(0..<2) {index in
                         
                         TopDeals()
+                    }
+                }
+            }
+            HStack{
+                Text("Featured products")
+                    .foregroundColor(.black)
+                    .font(.custom("Heavy", size: 20))
+                
+                Spacer()
+                Button(action:{},label: {
+                    Text("See all")
+                }).accentColor(Color(hex:"#CC9D76"))
+                
+            }.padding(.horizontal)
+            ScrollView(.horizontal,showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(0..<CategoryData.list1.count) {index in
+                        
+                        HomeCategory(data: CategoryData.list1[index])
                     }
                 }
             }
@@ -127,13 +146,18 @@ struct CategoryData {
         CategoryData(image: "rumili_kase", title: "Rumili Kase", price: "€2350"),
         
     ]
+    static let list1 = [
+        CategoryData(image: "SoteriaVazo", title: "Hagia Sophia Deesis", price: "€3450"),
+        CategoryData(image: "kavuk_vase", title: "Kavuk Vase", price: "€4250"),
+        CategoryData(image: "soteria_vase", title: "Soteria Vase", price: "€2950"),
+        
+    ]
 }
 struct TopDeals : View{
     var body: some View{
         ZStack(alignment:.leading){
-            
-            
-            LinearGradient(gradient: Gradient(colors: [ Color(hex:"B86518"),Color(hex:"#272833"),Color(hex:"#272833")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [ Color(hex:"B86518"),Color(hex:"#272833"),Color(hex:"#272833")]),
+                           startPoint: .topLeading, endPoint: .bottomTrailing)
                 .cornerRadius(4)
                 .frame(height: 160)
             HStack{
@@ -144,23 +168,24 @@ struct TopDeals : View{
                     Text("W:32cm H:26cm")
                         .font(.custom("Book", size: 14))
                         .foregroundColor(Color.white)
-                                 Text("€5650")
-                                 .foregroundColor(.white)
-                                 .font(.custom("Heavy", size: 24))
-                }.padding(.horizontal,8)
+                    Text("€5650")
+                        .foregroundColor(.white)
+                        .font(.custom("Heavy", size: 24))
+                }.padding(.horizontal)
                 Spacer()
                 Image("Gulcehre_ibrik")
                     .resizable()
                     .scaledToFit()
                     .frame(width:220, height:220)
-                .padding(-12)
-                    
+                    .padding(.trailing,-36)
+                
                 
             }
             
             
             
-        }.padding()
+        }.padding(.leading)
+        
     }
 }
 struct HomeCategory : View{
@@ -168,7 +193,9 @@ struct HomeCategory : View{
     var body: some View{
         VStack{
             Image(data.image)
-            
+            .resizable()
+            .scaledToFit()
+            .frame(width:138, height:138)
             
             Text(data.title)
                 .foregroundColor(.black)
